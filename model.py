ee122cf876e1630975b5ddbff2c358857c15331e
Kelvin Purdom <kelvin.purdom@gmail.com>
2023-02-06 14:39:12 +0100

    trying to set up streamlit app

---
+++ model.py
@@ -0,0 +1,46 @@
+import keras
+from keras.layers import Embedding, LSTM, Dense, Dropout, Bidirectional
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+import pandas as pd
+import csv
+from sklearn.model_selection import train_test_split
+from keras.callbacks import ModelCheckpoint
+import regex as re
+import numpy as np
+
+from keras.utils import np_utils
+import pickle
+
+max_sequence_len = 88
+model = keras.models.load_model('my_model.h5')
+with open('notebooks/tokenizer.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
+
+
+def make_lyrics(seed_text, next_words):
+    pred_index=[]
+    for i in range(next_words):
+        token_list = tokenizer.texts_to_sequences([seed_text])[0]
+        token_list = pad_sequences([token_list],
+                     maxlen=max_sequence_len-1,padding='pre')
+        #print(token_list.shape)
+        token_list = np.reshape(token_list, (1, max_sequence_len-1, 1))
+        predicted = model.predict(token_list, verbose=0)
+        predicted_index =  np.argmax(predicted)
+        pred_index.append(predicted_index)
+
+
+
+        #predicted_index=1
+        output_word = ""
+        for word, index in tokenizer.word_index.items():
+            if index == predicted_index:
+                output_word = word
+                break
+        seed_text += " " + output_word
+    #print(seed_text)
+    return seed_text
+
+make_lyrics('I', 23)
